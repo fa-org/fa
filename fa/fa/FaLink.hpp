6cba09d9913a816f21b9e636652661e04482eae9
fawdlstty <f@fawdlstty.com>
2021-06-25 17:30:17 +0800

    init

---
+++ fa/fa/FaLink.hpp
@@ -0,0 +1,14 @@
+#ifndef __FaLINK_HPP__
+#define __FaLINK_HPP__
+
+
+
+class FaLink {
+public:
+
+private:
+};
+
+
+
+#endif //__FaLINK_HPP__
---
+++ fa/fa/FaUseAST.hpp
@@ -0,0 +1,14 @@
+#ifndef __FAUSEAST_HPP__
+#define __FAUSEAST_HPP__
+
+
+
+class FaUseAST {
+public:
+
+private:
+};
+
+
+
+#endif //__FAUSEAST_HPP__
