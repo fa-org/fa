d33c7f05365f480c475d2501a35c877a65b6b1a1
Stacker <hidestudio@163.com>
2021-12-25 22:23:24 +0800

    Create UnitTest04_Loop.cs

---
+++ fa/fac.Test/UnitTest04_Loop.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace fac.Test
+{
+    [TestClass]
+    public class UnitTest04_Loop
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string code = @"
+
+use fa;
+
+class Program {
+	public static void Main () {
+		int id = 0;
+		while id<3 {
+			Console.WriteLine(""Hey, User{0}"".Format(id));
+            id += 1;
+		}
+	}
+}
+
+";
+            string ret = BuildTool.RunAndGetReturn(code);
+            Assert.AreEqual(ret, "Hey, User0\r\nHey, User1\r\nHey, User2\r\n");
+        }
+    }
+}
