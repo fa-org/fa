b1b2240ef5e05e0a23d536fc8332062f401660f2
萌萌哒帕克 <35246761+AdorableParker@users.noreply.github.com>
2021-12-27 21:04:08 +0800

    更新检查 修正格式

---
+++ fa/fac/CheckUpdates.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace fac {
+	public class Releases {
+
+		[JsonPropertyName("tag_name")]
+		public string ReleaseVersion {
+			get; set;
+		}
+
+		[JsonPropertyName("body")]
+		public string Detail {
+			get; set;
+		}
+		[JsonPropertyName("assets")]
+		public IList<Assets> Asset {
+			get; set;
+		}
+	}
+	public class Assets {
+		[JsonPropertyName("browser_download_url")]
+		public string Download_url {
+			get; set;
+		}
+	}
+}
+
